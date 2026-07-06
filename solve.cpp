55e42fe1877d1eb7392d7b2ad923c77ee54c3ad3
YousefElSayed19 <yousafe2042005@gmail.com>
2026-07-06 22:59:34 +0300

    B - Midoriya Hates Maths

---
+++ solve.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+
+void solve()
+{
+    long long a,b;
+    cin>>a>>b;
+    cout<<a*b<<endl;
+}
+
+int main()
+{
+    solve();
+}
