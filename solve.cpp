caf076252efb5a52f83bdc47fc6def1ee9d54bb5
YousefElSayed19 <yousafe2042005@gmail.com>
2026-07-08 22:57:14 +0300

    d

--- solve.cpp
+++ solve.cpp
@@ -2,21 +2,22 @@
 using namespace std;
 #define ll long long
 #define endl '\n'
-void io_st(){
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt", "r", stdin);
-        freopen("output.txt", "w", stdout);
-    #endif
-        ios_base::sync_with_stdio(false);
-        cin.tie(nullptr);
-        cout.tie(nullptr);
+void io_st()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 }
 void solve()
 {
-    string n;
-    cin>>n;
-    cout<<n[0]<<endl;1
-
+    int n, i;
+    cin >> n >> i;
+    cout << n - i + 1 << endl;
+}
 int main()
 {
     io_st();
