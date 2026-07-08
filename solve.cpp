8a1502055452f59e36a05f27398a1c79c6535139
YousefElSayed19 <yousafe2042005@gmail.com>
2026-07-08 23:07:08 +0300

    dsasd

--- solve.cpp
+++ solve.cpp
@@ -16,6 +16,13 @@ void solve()
 {
     string s;
     cin>>s;
+    ll sum =0;
+    for(int i = 0;i<s.size();i++){
+        if((s[i]-'0')%2!=0){
+            sum+=s[i]-'0';
+        }
+    }
+    cout<<sum<<endl;
 }
 int main()
 {
