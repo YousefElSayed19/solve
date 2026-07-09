b3d64298266e6b46fd47ac2b0bee2acf54c59bf2
YousefElSayed19 <yousafe2042005@gmail.com>
2026-07-09 21:25:52 +0300

    s

--- solve.cpp
+++ solve.cpp
@@ -14,14 +14,7 @@ void io_st()
 }
 void solve()
 {
-    int a,b;cin>>a>>b;
-    int c= 0 ;
-    while(a<=b){
-        a*=3;
-        b*=2;
-        c++;
-    }
-    cout<<c<<endl;
+    
 }
 int main()
 {
