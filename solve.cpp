e45ed52525c72916b3f52bd8f5ca1d6d14943fdf
YousefElSayed19 <yousafe2042005@gmail.com>
2026-07-10 23:16:42 +0300

    asad

--- solve.cpp
+++ solve.cpp
@@ -14,12 +14,19 @@ void io_st()
 }
 void solve()
 {
-    int a,b;cin>>a>>b;
-    int c= 0 ;
-    while(a<=b){
-        a*=3;
-        b*=2;
-        c++;
+    ll n;cin>>n;
+    int c= 0;
+    if(n%5!=0){
+        cout<<"NO"<<endl;
+        return ;
+    }else{
+        if(n>=15){
+            c+=n/15;
+            n%=15;
+        }
+        if(n>=5){
+            c++;
+        }
     }
     cout<<c<<endl;
 }
